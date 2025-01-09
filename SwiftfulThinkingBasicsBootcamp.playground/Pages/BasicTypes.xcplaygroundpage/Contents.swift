cbf88ed73af9d255235d554095f013c921a4296e
rob212 <robertmcbryde83@gmail.com>
2025-01-09 21:43:10 +0000

    initial commit

---
+++ SwiftfulThinkingBasicsBootcamp.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+// String is "regular text"
+let myFirstItem: String = "Hello, world!"
+
+// Reference previously create objects
+let myTitle = myFirstItem
+
+
+// Boolean (aka Bool) is true of false
+let mySecondItem: Bool = true
+let myThirdItem: Bool = false
+
+// Do not do this
+// use a Boolean
+// let myFourthItem: String = "true"
+
+// Swift is a type-safe language
+let myfifthItem: Bool = true
+let mySixthItem: String = "Hello, world"
+// Type inference
+let mySeventhItem = "Hello, world!"
+
+
+// Date
+let myFirstDate: Date = Date()
+
+
+// Number can be Int, Double, CGFloat & more
+
+// Int is a whole number
+let myFirstNumber: Int  = 1
+
+// Double is a decimal and used for math
+let mySecondNumber: Double = 1.0
+
+// Use CGFloat is for UI 
+let myThirdNumber: CGFloat = 1.1
+
+
---
+++ SwiftfulThinkingBasicsBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+print(greeting)
+print(greeting)
+print(greeting)
+
+
+// This is a comment
+
+/*
+ This is a multi-line comment.
+ across many lines
+ */
+
+
+
+// Naming Conventions
+
+// Camel Case
+let firstGreeting = "Hello, world!"
+let thisIsMyFirstGreeting = "Howdy!"
